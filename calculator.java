7aee9ac4cf07f839036d929310b9b3349b913143
aswin00000 <91382951+aswin00000@users.noreply.github.com>
2023-01-04 21:54:24 +0530

    Add files via upload
    etha sugamanoo

---
+++ calculator.java
@@ -0,0 +1,393 @@
+/*import javax.swing.*;
+import java.awt.event.*;
+
+public class calculator extends JFrame implements ActionListener 
+{
+    private JTextField t1;
+    private JButton b1;
+    private JButton b2;
+    private JButton b3;
+    private JButton b4;
+    private JButton b5;
+    private JButton b6;
+    private JButton b7;
+    private JButton b8;
+    private JButton b9;
+    private JButton b10;
+    private JButton b11;
+    private JButton b12;
+    private JButton b13;
+    private JButton b14;
+    private JButton b15;
+    private JButton b16;
+    
+    private Integer res;
+    private String Operation;
+
+    public calculator()
+    {
+        setLayout(null);
+        setSize(640,480);
+
+        t1 = new JTextField();
+        t1.setBounds(100,100,200,30);
+        b1 = new JButton("1");
+        b1.setBounds(100,140,50,30);
+        b2 = new JButton("2");
+        b2.setBounds(150,140,50,30);
+        b3 = new JButton("3");
+b3.setBounds(200,140,50,30);
+b4 = new JButton("+");
+b4.setBounds(250,140,50,30);
+// Third Row
+b5 = new JButton("4");
+b5.setBounds(100,170,50,30);
+b6 = new JButton("5");
+
+b6.setBounds(150,170,50,30);
+b7 = new JButton("6");
+b7.setBounds(200,170,50,30);
+b8 = new JButton("-");
+b8.setBounds(250,170,50,30);
+// Fourth Row
+b9 = new JButton("7");
+b9.setBounds(100,200,50,30);
+b10 = new JButton("8");
+b10.setBounds(150,200,50,30);
+b11 = new JButton("9");
+b11.setBounds(200,200,50,30);
+b12 = new JButton("*");
+b12.setBounds(250,200,50,30);
+// Fourth Row
+b13 = new JButton("/");
+b13.setBounds(100,230,50,30);
+b14 = new JButton("%");
+b14.setBounds(150,230,50,30);
+b15 = new JButton("=");
+b15.setBounds(200,230,50,30);
+b16 = new JButton("C");
+b16.setBounds(250,230,50,30);
+add(t1);add(b1);add(b2);
+add(b3);add(b4);add(b5);
+add(b6);add(b7);add(b8);
+add(b9);add(b10);add(b11);
+add(b12);add(b13);add(b14);
+add(b15);add(b16);
+b1.addActionListener(this);b2.addActionListener(this);
+b3.addActionListener(this);b4.addActionListener(this);
+b5.addActionListener(this);b6.addActionListener(this);
+b7.addActionListener(this);b8.addActionListener(this);
+b9.addActionListener(this);b10.addActionListener(this);
+b11.addActionListener(this);b12.addActionListener(this);
+b13.addActionListener(this);b14.addActionListener(this);
+b15.addActionListener(this);b16.addActionListener(this);
+    } 
+    public void doAction(String op)
+    {
+        if(Operation == null)
+        {
+            Operation = op;
+            res = Integer.parseInt(t1.getText());
+            t1.setText("");
+        }
+        else{
+            switch(Operation)
+            {
+                case "+": res = res + Integer.parseInt(t1.getText());
+
+break;
+
+case "-": res = res - Integer.parseInt(t1.getText());
+
+break;
+case "/": try{
+
+if(t1.getText().equals("0")){
+throw new
+
+ArithmeticException("Divide by Zero");
+
+}
+res = res / Integer.parseInt(t1.getText());
+
+}
+catch(ArithmeticException e){
+t1.setText(e.getMessage());
+Operation = null;
+res = 0;
+}
+break;
+
+case "*": res = res * Integer.parseInt(t1.getText());
+
+break;
+
+case "%": res = res % Integer.parseInt(t1.getText());
+
+break;
+            }
+            if(op.equals("=")){
+                t1.setText(res.toString());
+                res = 0;
+                Operation = null;
+                }
+                else{
+                Operation = op;
+                t1.setText("");
+                }
+        }
+    }
+   
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource()== b1)
+t1.setText(t1.getText()+"1");
+else if(e.getSource()== b2)
+t1.setText(t1.getText()+"2");
+else if(e.getSource()== b3)
+t1.setText(t1.getText()+"3");
+else if(e.getSource()== b5)
+t1.setText(t1.getText()+"4");
+else if(e.getSource()== b6)
+t1.setText(t1.getText()+"5");
+else if(e.getSource()== b7)
+t1.setText(t1.getText()+"6");
+else if(e.getSource()== b9)
+t1.setText(t1.getText()+"7");
+else if(e.getSource()== b10)
+t1.setText(t1.getText()+"8");
+else if(e.getSource()== b11)
+t1.setText(t1.getText()+"9");
+else if(e.getSource()== b16){
+t1.setText("");
+res =0;
+Operation = null;
+        
+    }
+    else if(e.getSource()== b4){
+        doAction("+");}
+        else if(e.getSource()== b8)
+doAction("-");
+else if(e.getSource()== b12)
+doAction("*");
+else if(e.getSource()== b13)
+doAction("/");
+else if(e.getSource()== b14)
+doAction("%");
+else if(e.getSource()== b15)
+
+doAction("=");
+
+}
+public static void main(String args[]){
+new calculator().setVisible(true);
+}
+}*/
+// Java program to create a simple calculator
+// with basic +, -, /, * using java swing elements
+ 
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+class calculator extends JFrame implements ActionListener {
+    // create a frame
+    static JFrame f;
+ 
+    // create a textfield
+    static JTextField l;
+ 
+    // store operator and operands
+    String s0, s1, s2;
+ 
+    // default constructor
+    calculator()
+    {
+        s0 = s1 = s2 = "";
+    }
+ 
+    // main function
+    public static void main(String args[])
+    {
+        // create a frame
+        f = new JFrame("calculator");
+ 
+        try {
+            // set look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+ 
+        // create a object of class
+        calculator c = new calculator();
+ 
+        // create a textfield
+        l = new JTextField(16);
+ 
+        // set the textfield to non editable
+        l.setEditable(false);
+ 
+        // create number buttons and some operators
+        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
+ 
+        // create number buttons
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
+ 
+        // equals button
+        beq1 = new JButton("=");
+ 
+        // create operator buttons
+        ba = new JButton("+");
+        bs = new JButton("-");
+        bd = new JButton("/");
+        bm = new JButton("*");
+        beq = new JButton("C");
+ 
+        // create . button
+        be = new JButton(".");
+ 
+        // create a panel
+        JPanel p = new JPanel();
+ 
+        // add action listeners
+        bm.addActionListener(c);
+        bd.addActionListener(c);
+        bs.addActionListener(c);
+        ba.addActionListener(c);
+        b9.addActionListener(c);
+        b8.addActionListener(c);
+        b7.addActionListener(c);
+        b6.addActionListener(c);
+        b5.addActionListener(c);
+        b4.addActionListener(c);
+        b3.addActionListener(c);
+        b2.addActionListener(c);
+        b1.addActionListener(c);
+        b0.addActionListener(c);
+        be.addActionListener(c);
+        beq.addActionListener(c);
+        beq1.addActionListener(c);
+ 
+        // add elements to panel
+        
+        p.add(l);
+        p.add(ba);
+        p.add(b1);
+        p.add(b2);
+        p.add(b3);
+        p.add(bs);
+        p.add(b4);
+        p.add(b5);
+        p.add(b6);
+        p.add(bm);
+        p.add(b7);
+        p.add(b8);
+        p.add(b9);
+        p.add(bd);
+        p.add(be);
+        p.add(b0);
+        p.add(beq);
+        p.add(beq1);
+
+ 
+        // set Background of panel
+        
+ 
+        // add panel to frame
+        f.add(p);
+ 
+        f.setSize(500, 500);
+        f.show();
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        String s = e.getActionCommand();
+ 
+        // if the value is a number
+        if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
+            // if operand is present then add to second no
+            if (!s1.equals(""))
+                s2 = s2 + s;
+            else
+                s0 = s0 + s;
+ 
+            // set the value of text
+            l.setText(s0 + s1 + s2);
+        }
+        else if (s.charAt(0) == 'C') {
+            // clear the one letter
+            s0 = s1 = s2 = "";
+ 
+            // set the value of text
+            l.setText(s0 + s1 + s2);
+        }
+        else if (s.charAt(0) == '=') {
+ 
+            double te;
+ 
+            // store the value in 1st
+            if (s1.equals("+"))
+                te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+            else if (s1.equals("-"))
+                te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+            else if (s1.equals("/"))
+                te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+            else
+                te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+ 
+            // set the value of text
+            l.setText(s0 + s1 + s2 + "=" + te);
+ 
+            // convert it to string
+            s0 = Double.toString(te);
+ 
+            s1 = s2 = "";
+        }
+        else {
+            // if there was no operand
+            if (s1.equals("") || s2.equals(""))
+                s1 = s;
+            // else evaluate
+            else {
+                double te;
+ 
+                // store the value in 1st
+                if (s1.equals("+"))
+                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+                else if (s1.equals("-"))
+                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+                else if (s1.equals("/"))
+                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                else
+                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+ 
+                // convert it to string
+                s0 = Double.toString(te);
+ 
+                // place the operator
+                s1 = s;
+ 
+                // make the operand blank
+                s2 = "";
+            }
+ 
+            // set the value of text
+            l.setText(s0 + s1 + s2);
+        }
+    }
+}
+
+
+
+
